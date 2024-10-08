d56d7ec4ec82e6ecde456a03cbad7a87bcee2f1b
01001100 <davidwhittingham94@gmail.com>
2024-10-08 15:31:15 +0200

    Add netlify form support.

--- src/components/molecules/FormBlock/index.tsx
+++ src/components/molecules/FormBlock/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useState } from 'react';
 import classNames from 'classnames';
 
 import { DynamicComponent } from '../../components-registry';
@@ -7,49 +8,66 @@ import { mapStylesToClassNames as mapStyles } from '../../../utils/map-styles-to
 export default function FormBlock(props) {
     const formRef = React.createRef<HTMLFormElement>();
     const { elementId, className, fields = [], submitLabel, styles = {} } = props;
+    const [status, setStatus] = useState(null);
+    const [error, setError] = useState(null);
 
     if (fields.length === 0) {
         return null;
     }
 
-    const handleFormSubmit = async (event) => {
+    async function handleSubmit(event) {
         event.preventDefault();
-        const formData = new FormData(event.target);
-        await fetch('/__forms.html', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-            body: new URLSearchParams(formData).toString()
-        });
-        // Success & error handling should come here
-    };
+        try {
+            setStatus('pending');
+            setError(null);
+            const data = new FormData(formRef.current);
+            const res = await fetch('/__forms.html', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+                body: new URLSearchParams(data as any).toString()
+            });
+            if (res.status === 200) {
+                setStatus('ok');
+            } else {
+                setStatus('error');
+                setError(`${res.status} ${res.statusText}`);
+            }
+        } catch (e) {
+            setStatus('error');
+            setError(`${e}`);
+        }
+    }
 
     return (
-        <>
-            <form name={elementId} data-netlify="true" hidden>
-                <input type="hidden" name="form-name" value={elementId} />
-                {fields.map((field, index) => (
-                    <input key={index} name={field.name} type="text" />
-                ))}
-            </form>
-            <form
-                className={classNames('sb-component', 'sb-component-block', 'sb-component-form-block', className)}
-                name={elementId}
-                id={elementId}
-                onSubmit={handleFormSubmit}
-                ref={formRef}
-            >
-                <div className="grid sm:grid-cols-2 sm:gap-x-4">
-                    <input type="hidden" name="form-name" value={elementId} />
-                    {fields.map((field, index) => {
-                        return <DynamicComponent key={index} {...field} />;
-                    })}
+        <form
+            className={classNames('sb-component', 'sb-component-block', 'sb-component-form-block', className)}
+            name={elementId}
+            id={elementId}
+            onSubmit={handleSubmit}
+            ref={formRef}
+            data-netlify="true"
+        >
+            <input type="hidden" name="form-name" value={elementId} />
+            <div className="grid sm:grid-cols-2 sm:gap-x-4">
+                {fields.map((field, index) => {
+                    return <DynamicComponent key={index} {...field} />;
+                })}
+            </div>
+            <div className={classNames('mt-4', styles.submitLabel?.textAlign ? mapStyles({ textAlign: styles.submitLabel?.textAlign }) : null)}>
+                <button type="submit" className="sb-component sb-component-block sb-component-button sb-component-button-primary" disabled={status === 'pending'}>
+                    {submitLabel}
+                </button>
+            </div>
+            {status === 'ok' && (
+                <div className="alert alert-success">
+                    Success! ✨
                 </div>
-                <div className={classNames('mt-4', styles.submitLabel?.textAlign ? mapStyles({ textAlign: styles.submitLabel?.textAlign }) : null)}>
-                    <button type="submit" className="sb-component sb-component-block sb-component-button sb-component-button-primary">
-                        {submitLabel}
-                    </button>
+            )}
+            {status === 'error' && (
+                <div className="alert alert-error">
+                    Error! 😵‍💫 {error} Please reach out via contact@shredgang.cc
                 </div>
-            </form>
-        </>
+            )}
+        </form>
     );
 }
